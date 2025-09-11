77659e1902ad6b348b9ba631385a8d1299d2f65e
Matheus França <mateozimfs@hotmail.com>
2025-09-11 11:24:31 -0300

    alterando installed apps da galeria

--- galeria/models.py
+++ galeria/models.py
@@ -8,4 +8,4 @@ class Fotografia(models.Model):
     foto = models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
-        return f"Fotografia [nome={self.name}]"
+        return f"Fotografia [nome={self.nome}]"
